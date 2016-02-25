d3affcb0d20fe8cf04d3d823692c6cdfad008843
Yahor Mikhnevich <elfenlaid@yandex-team.ru>
2016-02-25 17:02:02 +0300

    Add YandexMobileMetrica SDK 2.3.1

---
+++ YandexMobileMetrica.framework/Headers/YandexMobileMetrica.h
@@ -0,0 +1,15 @@
+/*
+ *  YandexMobileMetrica.h
+ *
+ * This file is a part of the AppMetrica
+ *
+ * Version for iOS © 2015 YANDEX
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://legal.yandex.com/metrica_termsofuse/
+ */
+
+#import <YandexMobileMetrica/YMMYandexMetrica.h>
+#import <YandexMobileMetrica/YMMVersion.h>
+#import <YandexMobileMetrica/YMMYandexMetricaConfiguration.h>
+#import <YandexMobileMetrica/YMMYandexMetricaPreloadInfo.h>
